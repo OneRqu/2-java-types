e293f1699860fb56497bcd6efeca8f3e209faa4a
Maksim <krukovmaksim1980@gmail.com>
2026-09-09 22:16:03 +0500

    Крюков Максим. Задание 06.

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,30 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long number = (long) x + y;
+        int count = 0;
 
-        return 0;
+        if (number == 0) {
+            return 1;
+        }
+
+        if (number < 0) {
+            number = -number;
+        }
+
+        while (number > 0) {
+            count++;
+            number /= 10;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
