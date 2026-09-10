bb9c91fd0f01513417d14bb961f55bf1f03a9e51
Maksim <krukovmaksim1980@gmail.com>
2026-09-10 23:38:41 +0500

    Крюков Максим. Задание 11.

--- task11/src/com/example/task11/Task11.java
+++ task11/src/com/example/task11/Task11.java
@@ -4,13 +4,12 @@ public class Task11 {
 
     public static float benefit(float sum, float percent) {
 
-        // TODO исправьте функцию, чтобы избежать накопления ошибки
-
-        // Считаем проценты за год
+        float totalRate = 0.0f;
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            totalRate = percent + totalRate + (totalRate * percent);
         }
-        return sum;
+
+        return sum + (sum * totalRate);
     }
 
     public static void main(String[] args) {
